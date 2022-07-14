1af990c267268a0e30a34d8077af555485202e72
Sergey <sergey030901@mail.ru>
2022-07-14 03:16:45 +0300

    overload function

--- app.ts
+++ app.ts
@@ -1,50 +1,14 @@
-enum PaymentStatus {
-	Success = "success",
-	Failed = "failed"
-}
-
-interface IPayment {
-	sum: number;
-	from: number;
-	to: number;
-}
-
-interface IPaymentRequest extends IPayment { }
-
-interface DataSuccess extends IPayment {
-	databaseId: number;
-}
-
-interface IDataFailed {
-	errorMessage: string,
-	errorCode: number;
-}
-
-interface IResponceSuccess {
-	status: PaymentStatus.Success;
-	data: DataSuccess;
-}
-
-interface IResponceFailed {
-	status: PaymentStatus.Failed;
-	data: IDataFailed;
-}
-
-
-type Res = IResponceSuccess | IResponceFailed;
-
-function isSuccess(res: Res): res is IResponceSuccess {
-	if (res.status === PaymentStatus.Success) {
-		return true
-	}
-	return false
-
-}
-
-function getIdFromData(res: Res): number {
-	if (isSuccess(res)) {
-		return res.data.databaseId
-	} else {
-		throw new Error(res.data.errorMessage)
+class User {
+	skills: string[];
+	addSkill(skill: string): void;
+	addSkill(skills: string[]): void;
+	addSkill(skillOrSkills: string | string[]) {
+		if (typeof skillOrSkills === 'string') {
+			this.skills.push(skillOrSkills)
+		} else {
+			this.skills.concat(skillOrSkills)
+		}
 	}
 }
+
+new User().addSkill("dev")
