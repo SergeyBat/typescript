df8a98bb0f5efcd6c1bf71870252d46cf9a9ca4a
Sergey <sergey030901@mail.ru>
2022-07-14 21:50:59 +0300

    abstract class test6

--- app.js
+++ app.js
@@ -1,61 +1,18 @@
 "use strict";
-class Product {
-    constructor(id, title, price) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
+class Logger {
+    printDate(date) {
+        this.log(date.toString());
     }
 }
-class Delivery {
-    constructor(date) {
-        this.date = date;
+class MyLogger extends Logger {
+    log(message) {
+        console.log(message);
+    }
+    logWithDate(message) {
+        let date = new Date;
+        this.printDate(date);
+        this.log(message);
     }
 }
-class HoumeDelivery extends Delivery {
-    constructor(date, address) {
-        super(date);
-        this.address = address;
-    }
-}
-class ShopDelivery extends Delivery {
-    constructor(shopId) {
-        super(new Date());
-        this.shopId = shopId;
-    }
-}
-class Cart {
-    constructor() {
-        this.products = [];
-    }
-    addProduct(product) {
-        this.products.push(product);
-    }
-    deleteProduct(productId) {
-        this.products = this.products.filter((p) => p.id !== productId);
-    }
-    getSum() {
-        return this.products
-            .map((p) => p.price)
-            .reduce((p1, p2) => p1 + p2);
-    }
-    setDelivery(delivery) {
-        this.delivery = delivery;
-    }
-    checkout() {
-        if (this.products.length == 0) {
-            throw new Error('Нет ни одного товара в корзине');
-        }
-        if (!this.delivery) {
-            throw new Error('Не указан способ доставки');
-        }
-        return { success: true };
-    }
-}
-const cart = new Cart();
-cart.addProduct(new Product(1, "Печенье", 10));
-cart.addProduct(new Product(2, "Торт", 30));
-cart.addProduct(new Product(3, "Шоколад", 20));
-cart.deleteProduct(1);
-cart.setDelivery(new HoumeDelivery(new Date, ""));
-console.log(cart.getSum());
-console.log(cart.checkout());
+const c = new MyLogger;
+c.logWithDate("Все отлично");
--- app.ts
+++ app.ts
@@ -1,72 +1,23 @@
-class Product {
-	constructor(
-		public id: number,
-		public title: string,
-		public price: number
-	) { }
+abstract class Logger {
+	abstract log(message: string): void;
 
-}
-
-class Delivery {
-	constructor(
-		public date: Date
-	) { }
-}
-
-class HoumeDelivery extends Delivery {
-	constructor(date: Date, public address: string) {
-		super(date)
+	printDate(date: Date) {
+		this.log(date.toString())
 	}
 }
 
-class ShopDelivery extends Delivery {
-	constructor(public shopId: number) {
-		super(new Date())
+class MyLogger extends Logger {
+	log(message: any): void {
+		console.log(message)
+	}
+
+	logWithDate(message: string){
+		let date = new Date;
+		this.printDate(date)
+		this.log(message)
+
 	}
 }
 
-
-type DeliveryOptions = HoumeDelivery | ShopDelivery;
-
-class Cart {
-	private products: Product[] = [];
-	private delivery: DeliveryOptions;
-
-
-	public addProduct(product: Product): void {
-		this.products.push(product);
-	}
-
-	public deleteProduct(productId: number): void {
-		this.products = this.products.filter((p: Product) => p.id !== productId);
-	}
-
-	public getSum(): number {
-		return this.products
-			.map((p: Product) => p.price)
-			.reduce((p1: number, p2: number) => p1 + p2)
-	}
-
-	public setDelivery(delivery: DeliveryOptions): void {
-		this.delivery = delivery
-	}
-
-	public checkout() {
-		if (this.products.length == 0) {
-			throw new Error('Нет ни одного товара в корзине');
-		}
-		if (!this.delivery) {
-			throw new Error('Не указан способ доставки');
-		}
-		return { success: true };
-	}
-}
-
-const cart = new Cart();
-cart.addProduct(new Product(1, "Печенье", 10));
-cart.addProduct(new Product(2, "Торт", 30));
-cart.addProduct(new Product(3, "Шоколад", 20));
-cart.deleteProduct(1);
-cart.setDelivery(new HoumeDelivery(new Date, ""))
-console.log(cart.getSum());
-console.log(cart.checkout());
+const c = new MyLogger;
+c.logWithDate("Все отлично")
